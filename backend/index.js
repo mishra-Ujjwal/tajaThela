a61e8c166014124fb2cf8cff0b1da7a503d09aff
mishra-Ujjwal <ujjwalmishra9717@gmail.com>
2025-10-12 14:56:34 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,39 +1,40 @@
 import express from "express";
 const app = express();
-import cors from "cors"
+import cors from "cors";
 import dotenv from "dotenv";
 import connectDb from "./config/mongoDb.js";
 import cookieParser from "cookie-parser";
-import {userRouter} from "./routes/user.route.js";
+import { userRouter } from "./routes/user.route.js";
 import vegetableRouter from "./routes/vegetable.route.js";
 import vendorRouter from "./routes/vendor.route.js";
 import cartRouter from "./routes/cart.route.js";
 import orderRouter from "./routes/order.route.js";
-dotenv.config();
-const port = process.env.PORT||5000;
-//cors
-app.use(cors({
-  origin:"https://tajathela-wy9b.onrender.com",
-  credentials:true,
-}))
-app.use(express.json()) //convert receiving data into json
-app.use(cookieParser())
 
+dotenv.config();
+const port = process.env.PORT || 5000;
+
+// CORS setup for production
+app.use(
+  cors({
+    origin: "https://tajathela-wy9b.onrender.com", // frontend URL
+    credentials: true, // allow cookies
+  })
+);
+
+app.use(express.json());
+app.use(cookieParser());
 app.use("/public", express.static("public"));
 
-//authrouters 
-app.use("/api/",userRouter)
-
-app.use("/vendor/",vegetableRouter)
-app.use("/owner/",vendorRouter)
+// Routers
+app.use("/api/", userRouter);
+app.use("/vendor/", vegetableRouter);
+app.use("/owner/", vendorRouter);
 app.use("/user/cart/", cartRouter);
 app.use("/user/", orderRouter);
-// access mongodb
-const connectDatabase =async()=>{
-  await connectDb()
-}
-connectDatabase()
-//start app
-app.listen(port, () => {
-  console.log("server is started");
-});
+
+// Connect MongoDB
+const connectDatabase = async () => await connectDb();
+connectDatabase();
+
+// Start server
+app.listen(port, () => console.log("Server is running on port " + port));
